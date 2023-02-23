afcf3a0015bbcc168114c2e4b2071f3ce7e7c837
Actav <actav@mail.com>
2023-02-23 21:43:31 +0300

    ADD: code written in c#

---
+++ csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Создать пустой список строк result.
+Перебрать все строки str в исходном массиве строк arr.
+Если длина строки str меньше или равна 3 символам, добавить ее в список result.
+Вернуть список result.
+*/
+
+string[] inputArray = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+string[] outputArray = GetShortStrings(inputArray);
+
+printResult(inputArray, outputArray);
+
+string[] GetShortStrings(string[] inputArray) {
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++) {
+        if (inputArray[i].Length <= 3) {
+            count++;
+        }
+    }
+    string[] outputArray = new string[count];
+    int index = 0;
+    for (int i = 0; i < inputArray.Length; i++) {
+        if (inputArray[i].Length <= 3) {
+            outputArray[index] = inputArray[i];
+            index++;
+        }
+    }
+    return outputArray;
+}
+
+void printResult(string[] inputArray, string[] outputArray) {
+    System.Console.Write($"[\"{String.Join("\", \"", inputArray)}\"] -> [\"{String.Join("\", \"", outputArray)}\"]\n");
+}
